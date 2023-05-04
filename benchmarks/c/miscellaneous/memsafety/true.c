9464c82b501a1c7dbfd65dc47430293be6d6ade2
Nico Pietzsch <n.pietzsch@tu-braunschweig.de>
2023-05-04 13:20:57 +0200

    changed benchmark

--- benchmarks/c/miscellaneous/memsafety/true.c
+++ benchmarks/c/miscellaneous/memsafety/true.c
@@ -17,13 +17,13 @@ int main()
     shared = malloc(sizeof(int));
     pthread_t t[NTHREADS];
 
-    for (int i = 0; i < NTHREADS; i++)
-        pthread_create(&t[i], 0, thread_n, (void *)i);
+    /*for (int i = 0; i < NTHREADS; i++)
+        //pthread_create(&t[i], 0, thread_n, (void *)i);*/
 
     free(shared);
 
-    for (int i = 0; i < NTHREADS; i++)
-        pthread_join(t[i], 0);
+    /*for (int i = 0; i < NTHREADS; i++)
+        pthread_join(t[i], 0);*/
 
     return 0;
 }

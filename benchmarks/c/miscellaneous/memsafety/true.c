3cc9fd65bc8b4b859cd579233f8a4c2f4066d86b
Nico Pietzsch <n.pietzsch@tu-braunschweig.de>
2023-05-04 13:07:13 +0200

    benchmarks

--- benchmarks/c/miscellaneous/memsafety/true.c
+++ benchmarks/c/miscellaneous/memsafety/true.c
@@ -1,8 +1,29 @@
 #include <stdlib.h>
+#include <pthread.h>
+
+#ifndef NTHREADS
+#define NTHREADS 1
+#endif
+
+int* shared;
 
 int main()
 {
-    int *a = malloc(sizeof(int));
-    *a = 5;
-    free(a);
+    shared = malloc(sizeof(int));
+    pthread_t t[NTHREADS];
+
+    for (int i = 0; i < NTHREADS; i++)
+        pthread_create(&t[i], 0, thread_n, (void *)i);
+
+    free(shared);
+
+    for (int i = 0; i < NTHREADS; i++)
+        pthread_join(t[i], 0);
+
+    return 0;
+}
+
+void *thread_n(void* arg)
+{
+    *shared = 42;
 }

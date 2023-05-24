0652fe94287cd09a15baa9fa0bb50d5c2ed06a03
Nico Pietzsch <n.pietzsch@tu-braunschweig.de>
2023-05-25 00:49:24 +0200

    minimal benchmark for graph

---
+++ benchmarks/c/miscellaneous/memsafety/minimal.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int *shared;
+
+void *t1_task() {
+    *shared = 42;
+}
+
+void *t2_task() {
+    int temp = *shared;
+    free(shared);
+}
+
+int main() {
+    shared = malloc(sizeof(int));
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, t1_task, NULL);
+    pthread_create(&t2, NULL, t2_task, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    int temp = *shared;
+    printf("%d\n", temp);
+}

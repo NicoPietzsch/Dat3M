799905549e85097c5741a016669ce2a50954fa6e
Nico Pietzsch <n.pietzsch@tu-braunschweig.de>
2023-05-25 01:11:21 +0200

    fixed unused variable

--- benchmarks/c/miscellaneous/memsafety/minimal.c
+++ benchmarks/c/miscellaneous/memsafety/minimal.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 #include <pthread.h>
 
-int *shared;
+int *shared1, *shared2;
 
 void *t1_task() {
-    *shared = 42;
+    *shared1 = 42;
 }
 
 void *t2_task() {
-    int temp = *shared;
-    free(shared);
+    shared2 = malloc(sizeof(int));
+    *shared2 = *shared1;
+    free(shared1);
 }
 
 int main() {
-    shared = malloc(sizeof(int));
+    shared1 = malloc(sizeof(int));
     pthread_t t1, t2;
     pthread_create(&t1, NULL, t1_task, NULL);
     pthread_create(&t2, NULL, t2_task, NULL);

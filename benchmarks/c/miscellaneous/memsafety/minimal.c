6f188105283812d1e4cdd9505de336f62e208755
Nico Pietzsch <n.pietzsch@tu-braunschweig.de>
2023-05-25 01:22:21 +0200

    simplyfied minimal example

--- benchmarks/c/miscellaneous/memsafety/minimal.c
+++ benchmarks/c/miscellaneous/memsafety/minimal.c
@@ -9,13 +9,13 @@ void *t1_task() {
 }
 
 void *t2_task() {
-    shared2 = malloc(sizeof(int));
     *shared2 = *shared1;
     free(shared1);
 }
 
 int main() {
     shared1 = malloc(sizeof(int));
+    shared2 = malloc(sizeof(int));
     pthread_t t1, t2;
     pthread_create(&t1, NULL, t1_task, NULL);
     pthread_create(&t2, NULL, t2_task, NULL);
